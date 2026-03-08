53fab736e94c222c84080cd52e5757237754e286
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-03-08 21:36:44 +0100

    t

---
+++ src/test/java/com/github/dimitryivaniuta/gateway/killswitch/KillSwitchApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.dimitryivaniuta.gateway.killswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Basic context boot test.
+ */
+@SpringBootTest
+class KillSwitchApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
